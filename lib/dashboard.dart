3ed6dc80a11f8b3ff20d1c1fa2085aff214c1bbd
prakhar <prakhar7557s@gmail.com>
2021-06-13 19:44:53 +0530

    homepage

---
+++ lib/dashboard.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+ 
+void main() => runApp(dash());
+
+class dash extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+    home: Scaffold(
+     
+        appBar: AppBar(
+           backgroundColor: Colors.teal,
+        title: Text("MyCovid Doctor's App")
+          ),
+        body: Center(
+          
+          child: ListSearch()
+          ),
+           
+            
+           floatingActionButton: FloatingActionButton(
+             backgroundColor: Colors.teal[600],
+          child: Icon(Icons.qr_code_2_rounded),
+          onPressed: () {
+            print("Clicked");
+            
+          },
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(16))),
+          elevation: 5,
+          highlightElevation: 10,
+          
+        ),
+        )
+      );
+  }
+}
+
+class ListSearch extends StatefulWidget {
+  ListSearchState createState() => ListSearchState();
+}
+
+class ListSearchState extends State<ListSearch> {
+
+  TextEditingController _textController = TextEditingController();
+
+  static List<String> mainDataList = [
+    
+    // from
+   "Apple",
+   "Apricot",
+   "Banana",
+   "Blackberry",
+   "Coconut",
+   "Date",
+   "Fig",
+   "Gooseberry",
+   "Grapes",
+   "Lemon",
+   "Litchi",
+   "Mango",
+   "Orange",
+   "Papaya",
+   "Peach",
+   "Pineapple",
+   "Pomegranate",
+   "Starfruit"
+  ];
+
+  // Copy Main List into New List.
+  List<String> newDataList = List.from(mainDataList);
+
+  onItemChanged(String value) {
+    setState(() {
+      newDataList = mainDataList
+      
+          .where((string) => string.toLowerCase().contains(value.toLowerCase()))
+          .toList();
+    });
+  }
+// to
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       backgroundColor: Colors.teal[50],
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: TextField(
+              controller: _textController,
+              decoration: InputDecoration(
+                hintText: 'Search by name or IPD',
+                // hintStyle: TextStyle( color: Colors.white),
+                              ),
+              onChanged: onItemChanged,
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              
+              padding: EdgeInsets.all(12.0),
+              children: newDataList.map((data) {
+                return ListTile(
+                  title: Text(data),
+                  
+                  onTap: ()=> print(data),);
+              }).toList(),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/home.dart
@@ -0,0 +1,33 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'dashboard.dart';
+
+class Home extends StatefulWidget {
+  // home page for doctor App after login (in process)
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  String uid;
+  @override
+  TextEditingController editingController = TextEditingController();
+  TabController controller;
+  List<String> listItems = [
+    // enter patient names here from database
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return dash();
+     
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    uid = FirebaseAuth.instance.currentUser.uid;
+  }
+}
